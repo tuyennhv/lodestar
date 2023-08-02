83f6ba7bcc03b3bb6a9ca4c46662991d6175faf4
Tuyen Nguyen <vutuyen2636@gmail.com>
2023-08-02 11:14:07 +0700

    chore: ensureWithinWeakSubjectivityPeriod

--- packages/state-transition/src/util/weakSubjectivity.ts
+++ packages/state-transition/src/util/weakSubjectivity.ts
@@ -111,6 +111,19 @@ export function isWithinWeakSubjectivityPeriod(
   wsState: BeaconStateAllForks,
   wsCheckpoint: Checkpoint
 ): boolean {
+  try {
+    ensureWithinWeakSubjectivityPeriod(config, wsState, wsCheckpoint);
+    return true;
+  } catch (_) {
+    return false;
+  }
+}
+
+export function ensureWithinWeakSubjectivityPeriod(
+  config: BeaconConfig,
+  wsState: BeaconStateAllForks,
+  wsCheckpoint: Checkpoint
+): void {
   const wsStateEpoch = computeCheckpointEpochAtStateSlot(wsState.slot);
   const blockRoot = getLatestBlockRoot(wsState);
   if (!ssz.Root.equals(blockRoot, wsCheckpoint.root)) {
@@ -123,5 +136,9 @@ export function isWithinWeakSubjectivityPeriod(
   }
   const wsPeriod = computeWeakSubjectivityPeriod(config, wsState);
   const clockEpoch = computeEpochAtSlot(getCurrentSlot(config, wsState.genesisTime));
-  return clockEpoch <= wsStateEpoch + wsPeriod;
+  if (clockEpoch > wsStateEpoch + wsPeriod) {
+    throw new Error(
+      `Fetched weak subjectivity checkpoint not within weak subjectivity period clockEpoch=${clockEpoch}, wsStateEpoch=${wsStateEpoch}, wsPeriod=${wsPeriod}`
+    );
+  }
 }
