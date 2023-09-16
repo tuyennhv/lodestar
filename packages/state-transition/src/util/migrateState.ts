91ebd31f1c1c1947570afa3d2002514e93f6d48f
Tuyen Nguyen <vutuyen2636@gmail.com>
2023-09-16 14:05:01 +0700

    fix: migrateState - set fields in a for loop

--- packages/state-transition/src/util/migrateState.ts
+++ packages/state-transition/src/util/migrateState.ts
@@ -14,180 +14,39 @@ export function migrateState(
   const allFields = Object.keys(stateType.fields);
   // genesisTime, could skip
   // genesisValidatorsRoot, could skip
-  const slotIndex = allFields.indexOf("slot");
-  const slotRange = fieldRanges[slotIndex];
-  clonedState.slot = stateType.fields.slot.deserialize(data.subarray(slotRange.start, slotRange.end));
-  // fork
-  const forkIndex = allFields.indexOf("fork");
-  const forkRange = fieldRanges[forkIndex];
-  clonedState.fork = stateType.fields.fork.deserializeToViewDU(data.subarray(forkRange.start, forkRange.end));
-  // latestBlockHeader
-  const latestBlockHeaderIndex = allFields.indexOf("latestBlockHeader");
-  const latestBlockHeaderRange = fieldRanges[latestBlockHeaderIndex];
-  clonedState.latestBlockHeader = stateType.fields.latestBlockHeader.deserializeToViewDU(
-    data.subarray(latestBlockHeaderRange.start, latestBlockHeaderRange.end)
-  );
-  // blockRoots
-  const blockRootsIndex = allFields.indexOf("blockRoots");
-  const blockRootsRange = fieldRanges[blockRootsIndex];
-  clonedState.blockRoots = stateType.fields.blockRoots.deserializeToViewDU(
-    data.subarray(blockRootsRange.start, blockRootsRange.end)
-  );
-  // stateRoots
-  const stateRootsIndex = allFields.indexOf("stateRoots");
-  const stateRootsRange = fieldRanges[stateRootsIndex];
-  clonedState.stateRoots = stateType.fields.stateRoots.deserializeToViewDU(
-    data.subarray(stateRootsRange.start, stateRootsRange.end)
-  );
-  // historicalRoots
-  const historicalRootsIndex = allFields.indexOf("historicalRoots");
-  const historicalRootsRange = fieldRanges[historicalRootsIndex];
-  clonedState.historicalRoots = stateType.fields.historicalRoots.deserializeToViewDU(
-    data.subarray(historicalRootsRange.start, historicalRootsRange.end)
-  );
-  // eth1Data
-  const eth1DataIndex = allFields.indexOf("eth1Data");
-  const eth1DataRange = fieldRanges[eth1DataIndex];
-  clonedState.eth1Data = stateType.fields.eth1Data.deserializeToViewDU(
-    data.subarray(eth1DataRange.start, eth1DataRange.end)
-  );
-  // eth1DataVotes
-  const eth1DataVotesIndex = allFields.indexOf("eth1DataVotes");
-  const eth1DataVotesRange = fieldRanges[eth1DataVotesIndex];
-  clonedState.eth1DataVotes = stateType.fields.eth1DataVotes.deserializeToViewDU(
-    data.subarray(eth1DataVotesRange.start, eth1DataVotesRange.end)
-  );
-  // eth1DepositIndex
-  const eth1DepositIndexIndex = allFields.indexOf("eth1DepositIndex");
-  const eth1DepositIndexRange = fieldRanges[eth1DepositIndexIndex];
-  clonedState.eth1DepositIndex = stateType.fields.eth1DepositIndex.deserialize(
-    data.subarray(eth1DepositIndexRange.start, eth1DepositIndexRange.end)
-  );
   // validators is loaded above
-  // balances
-  const balancesIndex = allFields.indexOf("balances");
-  const balancesRange = fieldRanges[balancesIndex];
-  clonedState.balances = stateType.fields.balances.deserializeToViewDU(
-    data.subarray(balancesRange.start, balancesRange.end)
-  );
-  // randaoMixes
-  // TODO: this takes 200ms to do hashTreeRoot, should we only update individual field?
-  const randaoMixesIndex = allFields.indexOf("randaoMixes");
-  const randaoMixesRange = fieldRanges[randaoMixesIndex];
-  clonedState.randaoMixes = stateType.fields.randaoMixes.deserializeToViewDU(
-    data.subarray(randaoMixesRange.start, randaoMixesRange.end)
-  );
-  // slashings
-  const slashingsIndex = allFields.indexOf("slashings");
-  const slashingsRange = fieldRanges[slashingsIndex];
-  clonedState.slashings = stateType.fields.slashings.deserializeToViewDU(
-    data.subarray(slashingsRange.start, slashingsRange.end)
-  );
-  // previousEpochParticipation
-  const previousEpochParticipationIndex = allFields.indexOf("previousEpochParticipation");
-  const previousEpochParticipationRange = fieldRanges[previousEpochParticipationIndex];
-  clonedState.previousEpochParticipation = stateType.fields.previousEpochParticipation.deserializeToViewDU(
-    data.subarray(previousEpochParticipationRange.start, previousEpochParticipationRange.end)
-  );
-  // currentEpochParticipation
-  const currentEpochParticipationIndex = allFields.indexOf("currentEpochParticipation");
-  const currentEpochParticipationRange = fieldRanges[currentEpochParticipationIndex];
-  clonedState.currentEpochParticipation = stateType.fields.currentEpochParticipation.deserializeToViewDU(
-    data.subarray(currentEpochParticipationRange.start, currentEpochParticipationRange.end)
-  );
-  // justificationBits
-  const justificationBitsIndex = allFields.indexOf("justificationBits");
-  const justificationBitsRange = fieldRanges[justificationBitsIndex];
-  clonedState.justificationBits = stateType.fields.justificationBits.deserializeToViewDU(
-    data.subarray(justificationBitsRange.start, justificationBitsRange.end)
-  );
-  // previousJustifiedCheckpoint
-  const previousJustifiedCheckpointIndex = allFields.indexOf("previousJustifiedCheckpoint");
-  const previousJustifiedCheckpointRange = fieldRanges[previousJustifiedCheckpointIndex];
-  clonedState.previousJustifiedCheckpoint = stateType.fields.previousJustifiedCheckpoint.deserializeToViewDU(
-    data.subarray(previousJustifiedCheckpointRange.start, previousJustifiedCheckpointRange.end)
-  );
-  // currentJustifiedCheckpoint
-  const currentJustifiedCheckpointIndex = allFields.indexOf("currentJustifiedCheckpoint");
-  const currentJustifiedCheckpointRange = fieldRanges[currentJustifiedCheckpointIndex];
-  clonedState.currentJustifiedCheckpoint = stateType.fields.currentJustifiedCheckpoint.deserializeToViewDU(
-    data.subarray(currentJustifiedCheckpointRange.start, currentJustifiedCheckpointRange.end)
-  );
-  // finalizedCheckpoint
-  const finalizedCheckpointIndex = allFields.indexOf("finalizedCheckpoint");
-  const finalizedCheckpointRange = fieldRanges[finalizedCheckpointIndex];
-  clonedState.finalizedCheckpoint = stateType.fields.finalizedCheckpoint.deserializeToViewDU(
-    data.subarray(finalizedCheckpointRange.start, finalizedCheckpointRange.end)
-  );
   // inactivityScores
-  // TODO: this takes ~500 to hashTreeRoot, should we only update individual field?
+  // this takes ~500 to hashTreeRoot, should we only update individual field?
   const inactivityScoresIndex = allFields.indexOf("inactivityScores");
   const inactivityScoresRange = fieldRanges[inactivityScoresIndex];
-  // clonedState.inactivityScores = stateType.fields.inactivityScores.deserializeToViewDU(
-  //   data.subarray(inactivityScoresRange.start, inactivityScoresRange.end)
-  // );
   loadInactivityScores(clonedState, data.subarray(inactivityScoresRange.start, inactivityScoresRange.end));
-  // TODO: 2 states could be in same sync committee, do a check before update
-  // this takes ~200ms to hashTreeRoot
-  // currentSyncCommittee
-  const currentSyncCommitteeIndex = allFields.indexOf("currentSyncCommittee");
-  const currentSyncCommitteeRange = fieldRanges[currentSyncCommitteeIndex];
-  clonedState.currentSyncCommittee = stateType.fields.currentSyncCommittee.deserializeToViewDU(
-    data.subarray(currentSyncCommitteeRange.start, currentSyncCommitteeRange.end)
-  );
-  // nextSyncCommittee
-  const nextSyncCommitteeIndex = allFields.indexOf("nextSyncCommittee");
-  const nextSyncCommitteeRange = fieldRanges[nextSyncCommitteeIndex];
-  clonedState.nextSyncCommittee = stateType.fields.nextSyncCommittee.deserializeToViewDU(
-    data.subarray(nextSyncCommitteeRange.start, nextSyncCommitteeRange.end)
-  );
-  // latestExecutionPayloadHeader
-  const latestExecutionPayloadHeaderIndex = allFields.indexOf("latestExecutionPayloadHeader");
-  const latestExecutionPayloadHeaderRange = fieldRanges[latestExecutionPayloadHeaderIndex];
-  clonedState.latestExecutionPayloadHeader = stateType.fields.latestExecutionPayloadHeader.deserializeToViewDU(
-    data.subarray(latestExecutionPayloadHeaderRange.start, latestExecutionPayloadHeaderRange.end)
-  );
-  // nextWithdrawalIndex
-  const nextWithdrawalIndexIndex = allFields.indexOf("nextWithdrawalIndex");
-  const nextWithdrawalIndexRange = fieldRanges[nextWithdrawalIndexIndex];
-  clonedState.nextWithdrawalIndex = stateType.fields.nextWithdrawalIndex.deserialize(
-    data.subarray(nextWithdrawalIndexRange.start, nextWithdrawalIndexRange.end)
-  );
-  // nextWithdrawalValidatorIndex
-  const nextWithdrawalValidatorIndexIndex = allFields.indexOf("nextWithdrawalValidatorIndex");
-  const nextWithdrawalValidatorIndexRange = fieldRanges[nextWithdrawalValidatorIndexIndex];
-  clonedState.nextWithdrawalValidatorIndex = stateType.fields.nextWithdrawalValidatorIndex.deserialize(
-    data.subarray(nextWithdrawalValidatorIndexRange.start, nextWithdrawalValidatorIndexRange.end)
-  );
-  // historicalSummaries
-  const historicalSummariesIndex = allFields.indexOf("historicalSummaries");
-  const historicalSummariesRange = fieldRanges[historicalSummariesIndex];
-  clonedState.historicalSummaries = stateType.fields.historicalSummaries.deserializeToViewDU(
-    data.subarray(historicalSummariesRange.start, historicalSummariesRange.end)
-  );
+  for (const [fieldName, type] of Object.entries(stateType.fields)) {
+    const field = fieldName as keyof typeof stateType.fields;
+    if (
+      // same to all states
+      field === "genesisTime" ||
+      field === "genesisValidatorsRoot" ||
+      // loaded above
+      field === "validators" ||
+      field === "inactivityScores"
+    ) {
+      continue;
+    }
+    const fieldIndex = allFields.indexOf(field);
+    const fieldRange = fieldRanges[fieldIndex];
+    if (type.isBasic) {
+      clonedState[field] = type.deserialize(data.subarray(fieldRange.start, fieldRange.end)) as never;
+    } else {
+      clonedState[field] = type.deserializeToViewDU(data.subarray(fieldRange.start, fieldRange.end)) as never;
+    }
+  }
   clonedState.commit();
-  // TODO: do a for loop
-  // for (const [fieldName, type] of Object.entries(stateType.fields)) {
-  //   const field = fieldName as keyof typeof stateType.fields;
-  //   if (field === "validators") {
-  //     continue;
-  //   }
-  //   const fieldIndex = allFields.indexOf(field);
-  //   const fieldRange = fieldRanges[fieldIndex];
-  //   if (field === "slot") {
-  //     clonedState[field] = type.deserialize(data.subarray(fieldRange.start, fieldRange.end));
-  //   }
-  //   if (type instanceof CompositeType) {
-  //     clonedState[field] = type.deserializeToViewDU(data.subarray(fieldRange.start, fieldRange.end));
-  //   } else if (type instanceof UintNumberType) {
-  //     clonedState[fieldName] = type.deserialize(data.subarray(fieldRange.start, fieldRange.end));
-  //   }
-  // }
 
   return clonedState;
 }
 
 // state store inactivity scores of old seed state, we need to update it
+// this value rarely changes even after 3 months of data as monitored on mainnet in Sep 2023
 function loadInactivityScores(
   state: CompositeViewDU<typeof ssz.capella.BeaconState>,
   inactivityScoresBytes: Uint8Array
